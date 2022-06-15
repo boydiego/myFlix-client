77afb277f13632266da49efced109f92dedc9313
Diego <diegolive5000@gmail.com>
2022-06-15 14:08:53 +0200

    add reacts-bootstrap to the login form

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
 
 export function LoginView(props) {
   const [ username, setUsername ] = useState('');
@@ -12,21 +14,25 @@ export function LoginView(props) {
   }
 
   return (
-    <form>
-      <label>
-        Username:
-        <input type="text" value={username} onChange={event => setUsername(event.target.value)} />
-      </label>
-      
-      <label>
-        Password:
-        <input type="password" value={password} onChange={event => setPassword(event.target.value)} />
-      </label>
+    <Form>
+      <Form.Group controlId='formUsername'>
+        <Form.Label>Username</Form.Label>
+        <Form.Control type="text" onChange={event => setUsername(event.target.value)} />    
+      </Form.Group>
 
-      <button type="submit" onClick={handleSubmit}>Submit</button>
+      <Form.Group controlId='formPassword'>
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" onChange={event => setPassword(event.target.value)} />
+      </Form.Group>
 
-      <button type="button">Register</button>
-    </form>
+      <Button variant='primary' type='submit' onClick={handleSubmit}>
+        Submit
+      </Button>
+
+      <Button variant='Secondary' type='link'>
+        Register
+      </Button>
+    </Form>
     
   )
 }
